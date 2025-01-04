f1663fb2bfe96129ecab267d2214a4a6d49c0da2
Yash Thipsay <yashthpsay@gmail.com>
2025-01-04 10:14:37 +0530

    authentication feature auth0

--- react-backend-app/client/src/index.js
+++ react-backend-app/client/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={'dev-veq6ppuvr633oybb.us.auth0.com'}
+      clientId={'maV0dZmjJ5rDMaLGt6vfjmJ0VugXzXsF'}
+      redirectUri={window.location.origin}
+      audience={`https://dev-veq6ppuvr633oybb.us.auth0.com/api/v2/`}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+        <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
