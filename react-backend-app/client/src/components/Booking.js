e78c51509b3139b1e7e9f7abc33e7ea34aa9ad38
Yash Thipsay <yashthpsay@gmail.com>
2025-01-06 13:33:30 +0530

    socketio

--- react-backend-app/client/src/components/Booking.js
+++ react-backend-app/client/src/components/Booking.js
@@ -21,16 +21,6 @@ function Booking() {
     );
   }
 
-  const timeSlots = [
-    { id: 1, time: "09:00 - 10:00" },
-    { id: 2, time: "10:00 - 11:00" },
-    { id: 3, time: "11:00 - 12:00" },
-    { id: 4, time: "12:00 - 13:00" },
-    { id: 5, time: "13:00 - 14:00" },
-    { id: 6, time: "14:00 - 15:00" },
-    { id: 7, time: "15:00 - 16:00" },
-    { id: 8, time: "16:00 - 17:00" },
-  ];
 
   const handleSlotChange = (slotId) => {
     setSelectedSlots((prev) =>
