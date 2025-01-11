1bcb016831b820a59f001ca4fc10b02442f74731
Yash Thipsay <yashthpsay@gmail.com>
2025-01-11 10:19:03 +0530

    payment verification controller

--- react-backend-app/server/controller/paymentController.js
+++ react-backend-app/server/controller/paymentController.js
@@ -1,4 +1,5 @@
 const {instance} = require('../utils/razorpayInstance');
+const crypto = require('crypto');
 const checkout = async (req, res) => {
 
     const {amount} = req.body;
@@ -13,6 +14,29 @@ const checkout = async (req, res) => {
 }
 
 const paymentVerification = async (req, res) => {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+    const secret = process.env.RAZORPAY_API_SECRET;
+
+      // Create the expected signature
+  const generated_signature = crypto
+  .createHmac('sha256', secret)
+  .update(razorpay_order_id + '|' + razorpay_payment_id)
+  .digest('hex');
+
+    // Compare the generated signature with the received signature
+    if (generated_signature === razorpay_signature) {
+        // Payment is successful
+        res.status(200).json({
+          success: true,
+          message: 'Payment verified successfully',
+        });
+      } else {
+        // Payment verification failed
+        res.status(400).json({
+          success: false,
+          message: 'Payment verification failed',
+        });
+      }
     
 }
-module.exports = {checkout}
+module.exports = {checkout, paymentVerification}
--- react-backend-app/server/routes/paymentRoutes.js
+++ react-backend-app/server/routes/paymentRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { checkout } = require('../controller/paymentController');
+const { checkout, paymentVerification } = require('../controller/paymentController');
 
 router.route("/checkout").post(checkout);
+router.route('/verify').post(paymentVerification);
 
 module.exports = router;
