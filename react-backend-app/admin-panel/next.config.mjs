43c20ec9e559f0edb8f2f2c7bc05d2667a5c495c
Yash Thipsay <yashthpsay@gmail.com>
2025-01-18 19:58:51 +0530

    dockerize and test working of the application

--- react-backend-app/admin-panel/next.config.mjs
+++ react-backend-app/admin-panel/next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-      AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-      AUTH0_REDIRECT_URI: process.env.AUTH0_REDIRECT_URI,
-      AUTH0_POST_LOGOUT_REDIRECT_URI: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI,
-    },
 };
 
 export default nextConfig;
