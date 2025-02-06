d8efc600da4303b3a0c70e5df939b8768b4aea62
Yash Thipsay <yashthpsay@gmail.com>
2025-02-06 09:13:46 +0530

    notifications

--- react-backend-app/admin-panel/app/page.jsx
+++ react-backend-app/admin-panel/app/page.jsx
@@ -2,8 +2,41 @@
 import { Navbar } from './components/navbar'
 import { Sidebar } from './components/sidebar'
 import { LandingContent } from './components/landing-content'
+import OneSignal from "react-onesignal";
+import {useState, useEffect} from 'react'
 
 export default function HomePage() {
+  useEffect(() => {
+    const initOneSignal = async () => {
+      try {
+        
+        await OneSignal.init({
+          appId: "c5c4fe85-5fe4-42e8-aaf4-c8edbfe967a9",
+          notifyButton: {
+            enable: true,
+          },
+          allowLocalhostAsSecureOrigin: true,    
+        });
+        const deviceState = OneSignal.User.PushSubscription
+        console.log("Device State:", deviceState);
+        const isEnabled = window.OneSignal.Notifications.isPushSupported();
+        console.log("Push notifications enabled:", isEnabled);
+
+        const userId =  OneSignal.User.PushSubscription.id;
+        console.log("User ID:", userId);
+      } catch (error) {
+        console.error("Error initializing OneSignal:", error);
+      }
+    };
+
+    initOneSignal();
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.OneSignal = undefined;
+      }
+    };
+  }, []);
+
   return (
     <main className="relative min-h-screen bg-cover bg-center bg-[url('https://gigsaw.s3.eu-north-1.amazonaws.com/3d-music-related-scene.jpg')]">
       {/* Dark overlay to prevent text overlap */}
