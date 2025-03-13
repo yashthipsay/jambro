0788cffb86eaced9a7ecbaed896d7de4b97ec8f6
Yash Thipsay <yashthpsay@gmail.com>
2025-03-13 14:44:04 +0530

    webhooks setup

--- react-backend-app/server/index.js
+++ react-backend-app/server/index.js
@@ -13,6 +13,7 @@ const bnkVerification = require("./routes/bnkVerification");
 const payoutRoutes = require("./routes/payoutRoutes");
 const subscriptionRoutes = require('./routes/subscriptions/subscriptionRoutes');
 const authRoutes = require("./routes/authRoutes");
+const webhookRoutes = require("./routes/webhooks/webhookRouter");
 const skuRoutes = require("./routes/subscriptions/skuAdminRoutes");
 const createReservationRouter = require("./routes/reservationRoutes");
 const Booking = require("./models/BookingSchema");
@@ -48,6 +49,7 @@ app.use('/api/spotify', spotifyRoutes);
 app.use("/api/auth", authRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
 app.use('/api/skus', skuRoutes);
+app.use('/api/webhooks', webhookRoutes);
 app.use('/api/reservations', createReservationRouter(io));
 app.use("/proxy", async (req, res) => {
   console.log("start");
